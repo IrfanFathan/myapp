33da63692dae2c816b0d1c28668e49a23c0d3809
muhammadirfanvnb <muhammadirfanvnb@gmail.com>
2024-05-17 16:55:05 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title:const Text('Hai'),
+        ),
+        body: Center(
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2929352777.
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+
+
